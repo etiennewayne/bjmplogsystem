9d5e4ba879975ac000361bcb63c348facb2e2517
eshen <etiennewayne@gmail.com>
2022-02-01 19:40:49 +0800

    update repo seeder

--- resources/views/administrator/admin-dashboard.blade.php
+++ resources/views/administrator/admin-dashboard.blade.php
@@ -2,14 +2,8 @@
 
 @section('content')
 
-    <navbar-admin  }}"></navbar-admin>
+    <navbar-admin firstname="{{ auth()->user()->fname }}"></navbar-admin>
     <dashboard-page></dashboard-page>
 
 @endsection
 
-        <script>
-            import NavbarAdmin from "../../js/components/Administrator/NavbarAdmin";
-            export default {
-                components: {NavbarAdmin}
-            }
-        </script>
